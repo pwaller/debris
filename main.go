94eda19fba9e2334d12e3bd4c55c667df861b20b
Peter Waller <peter@scraperwiki.com>
2013-11-23 00:20:16 +0000

    Many changes. Have shadows now.

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"os"
 	"unsafe"
 
+	"image"
+	// "image/color"
+	"image/png"
+
 	"github.com/go-gl/gl"
 	"github.com/go-gl/glfw"
 	"github.com/go-gl/glh"
@@ -21,35 +25,55 @@ import (
 type Planetoid struct {
 	apogee, perigee, inclination, phase0, phase, rising_node float64
 
-	radius float64
+	radius float32
 
-	quadric unsafe.Pointer
-	circle  *glh.MeshBuffer
+	// quadric unsafe.Pointer
+	circle *glh.MeshBuffer
+}
+
+var quadric unsafe.Pointer
+
+// Render a sphere with the correct orientation (poles along y-axis)
+// Also makes a correction to avoid lighting flicker.
+// TODO: Replace with geodesic
+func Sphere(radius float32, granularity int) {
+	glh.With(glh.Matrix{gl.MODELVIEW}, func() {
+		// Needed to avoid a strange lighting effect
+		gl.Rotatef(90, 0, 1, 0)
+
+		// Rotate so that poles are on the y-axis
+		gl.Rotatef(90, 1, 0, 0)
+		glu.Sphere(quadric, radius, granularity, granularity)
+	})
 }
 
 func (p *Planetoid) Render(dp float64) {
-	gl.PushMatrix()
+	glh.With(glh.Matrix{gl.MODELVIEW}, func() {
 
-	gl.Rotated(p.rising_node, 0, 1, 0)
-	gl.Rotated(p.inclination, 0, 0, 1)
+		gl.Rotated(p.rising_node, 0, 1, 0)
+		gl.Rotated(p.inclination, 0, 0, 1)
 
-	gl.Rotated(p.phase0+p.phase, 0, 1, 0)
-	p.phase += dp
+		gl.Rotated(p.phase0+p.phase, 0, 1, 0)
+		p.phase += dp
 
-	gl.PushMatrix()
-	gl.Translated(p.apogee, 0, 0)
-	glu.Sphere(p.quadric, float32(p.radius), 20, 20)
-	gl.PopMatrix()
+		glh.With(glh.Matrix{gl.MODELVIEW}, func() {
+			// TODO: Compute position correctly
+			gl.Translated(p.apogee, 0, 0)
+			Sphere(p.radius, 20)
+		})
 
-	gl.PushMatrix()
-	gl.Rotated(90, 1, 0, 0)
-	gl.Scaled(p.apogee, p.apogee, 1)
-	gl.Disable(gl.LIGHTING)
-	p.circle.Render(gl.LINE_STRIP)
-	gl.Enable(gl.LIGHTING)
-	gl.PopMatrix()
+		// TODO: Avoid depth thrashing when trails overlap
+		// Trail
+		glh.With(glh.Matrix{gl.MODELVIEW}, func() {
+			gl.Rotated(90, 1, 0, 0)
+			gl.Scaled(p.apogee, p.apogee, 1)
 
-	gl.PopMatrix()
+			gl.LineWidth(2)
+			glh.With(glh.Disable(gl.LIGHTING), func() {
+				p.circle.Render(gl.LINE_STRIP)
+			})
+		})
+	})
 }
 
 func main() {
@@ -70,63 +94,61 @@ func main() {
 	defer glfw.CloseWindow()
 
 	glfw.SetSwapInterval(1)
-	glfw.SetWindowTitle("Simple GLFW window")
+	glfw.SetWindowTitle("Debris")
 
-	glfw.SetWindowSizeCallback(onResize)
-	glfw.SetWindowCloseCallback(onClose)
-	glfw.SetMouseButtonCallback(onMouseBtn)
-	glfw.SetMouseWheelCallback(onMouseWheel)
-	glfw.SetKeyCallback(onKey)
-	glfw.SetCharCallback(onChar)
-
-	q := glu.NewQuadric()
+	quadric = glu.NewQuadric()
 
 	gl.Enable(gl.CULL_FACE)
+
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LEQUAL)
+
+	gl.Enable(gl.NORMALIZE)
 
 	gl.Enable(gl.BLEND)
 	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
 
 	gl.ShadeModel(gl.SMOOTH)
-	// gl.Enable(gl.LIGHTING_BIT)
 	gl.Enable(gl.LIGHTING)
-	// gl.LightModelf(gl.LIGHT_MODEL_AMBIENT, 0.5)
-	// gl.Materialf(face, pname, param)
-	// gl.L
-	// gl.Lightf(gl.LIGHT0, gl.LIGHT0, 0)
-	// log.Printf("Blah: %v", gl.GetError())
-	var (
-		ambient  []float32 = []float32{0.1, 0.3, 0.6, 1} // ambient light colour.
-		diffuse  []float32 = []float32{1, 1, 0.5, 1}     // diffuse light colour.
-		specular []float32 = []float32{1, 1, 1, 1}       // diffuse light colour.
-		lightpos []float32 = []float32{100000, 0, 0, 1}  // Position of light source.
 
-		mat_specular   []float32 = []float32{1, 1, 1, 1} //0.5, 0.5, 0.9, 0.5}
-		mat_shininess  []float32 = []float32{5}
-		light_position []float32 = []float32{0.0, 1.0, 0.0, 0.0}
+	var (
+		ambient        = []float32{0.1, 0.3, 0.6, 1}
+		diffuse        = []float32{1, 1, 0.5, 1}
+		specular       = []float32{0.4, 0.4, 0.4, 1}
+		light_position = []float32{1, 0, 0, 0}
+
+		// mat_specular  []float32 = []float32{1, 1, 0.5, 1}
+		mat_specular  = []float32{1, 1, 0.75, 1}
+		mat_shininess = float32(120)
+		// light_position []float32 = []float32{0.0, 0.0, 1.0, 0.0}
+	)
+
+	const (
+		fov               = 1 // degrees
+		znear             = 145
+		zfar              = 155
+		camera_z_offset   = -150
+		camera_x_rotation = 0 // degrees
+		// camera_x_rotation = 20 // degrees
+
+		faces        = 1000
+		earth_radius = 1
 	)
 
 	gl.Lightfv(gl.LIGHT1, gl.AMBIENT, ambient)
 	gl.Lightfv(gl.LIGHT1, gl.DIFFUSE, diffuse)
-	gl.Lightfv(gl.LIGHT1, gl.POSITION, lightpos)
-	// gl.Enable(gl.LIGHT1)
+	gl.Lightfv(gl.LIGHT1, gl.SPECULAR, specular)
+	gl.Lightfv(gl.LIGHT1, gl.POSITION, light_position)
+	gl.Enable(gl.LIGHT1)
 
-	// gl.LightModeli(gl.LIGHT_MODEL_LOCAL_VIEWER, gl.TRUE)
-	// gl.LightModeli(gl.LIGHT_MODEL_LOCAL_VIEWER, gl.FALSE)
-
+	mat_emission := []float32{0, 0, 0.1, 1}
+	gl.Materialfv(gl.FRONT_AND_BACK, gl.EMISSION, mat_emission)
 	gl.Materialfv(gl.FRONT_AND_BACK, gl.SPECULAR, mat_specular)
-	gl.Materialfv(gl.FRONT_AND_BACK, gl.SHININESS, mat_shininess)
+	gl.Materialf(gl.FRONT_AND_BACK, gl.SHININESS, mat_shininess)
 
-	gl.Lightfv(gl.LIGHT2, gl.SPECULAR, specular)
-	gl.Lightfv(gl.LIGHT2, gl.POSITION, light_position)
-	gl.Enable(gl.LIGHT2)
-
-	gl.MatrixMode(gl.MODELVIEW)
-
-	gl.ClearColor(0, 0, 0, 1)
+	gl.ClearColor(0.02, 0.02, 0.02, 1)
 	gl.ClearDepth(1)
-
+	gl.ClearStencil(0)
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 	b := createBuffer()
@@ -142,9 +164,9 @@ func main() {
 			inclination: 0,
 			phase0:      float64(i) * 57.1,
 			phase:       0,
-			radius:      r,
-			quadric:     glu.NewQuadric(),
-			circle:      b,
+			radius:      float32(r),
+			// quadric:     glu.NewQuadric(),
+			circle: b,
 		}
 		planetoids = append(planetoids, p)
 	}
@@ -152,112 +174,207 @@ func main() {
 	// Initial projection matrix:
 
 	gl.MatrixMode(gl.PROJECTION)
-
-	const (
-		fov   = 1 // degrees
-		znear = 0.1
-		zfar  = 300
-	)
 	glu.Perspective(fov, float64(w)/float64(h), znear, zfar)
-
-	const (
-		camera_z_offset   = -150
-		camera_x_rotation = 0 // degrees
-		// camera_x_rotation = 20 // degrees
-	)
-	gl.Translated(0, 0, camera_z_offset)
-	gl.Rotated(camera_x_rotation, 1, 0, 0)
-
-	gl.Rotated(90, 1, 0, 0)
 
 	d := float64(0)
 
+	wireframe := false
+	atmosphere := true
+	polar := false
+	rotating := false
+	front := false
+	earth := true
+	cone := true
 	running := true
-	for running {
-		glfw.SwapBuffers()
 
-		if glfw.Key(glfw.KeyF2) == 1 {
-			println("Screenshot captured")
-			glh.CaptureToPng("screenshot.png")
+	glfw.SetKeyCallback(func(key, state int) {
+		if state != glfw.KeyPress {
+			// Don't act on key coming up
+			return
 		}
 
-		running = (glfw.Key(glfw.KeyEsc) == 0 &&
-			glfw.WindowParam(glfw.Opened) == 1)
+		switch key {
+		case 'A':
+			atmosphere = !atmosphere
+		case 'C':
+			cone = !cone
+		case 'E':
+			earth = !earth
+		case 'R':
+			rotating = !rotating
+		case 'F':
+			front = !front
+			if front {
+				gl.FrontFace(gl.CW)
+			} else {
+				gl.FrontFace(gl.CCW)
+			}
+		case 'W':
+			wireframe = !wireframe
+			method := gl.GLenum(gl.FILL)
+			if wireframe {
+				method = gl.LINE
+			}
+			gl.PolygonMode(gl.FRONT_AND_BACK, method)
 
-		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		case glfw.KeyF2:
+			println("Screenshot captured")
+			// glh.CaptureToPng("screenshot.png")
 
-		// Rotate the planet
-		gl.MatrixMode(gl.PROJECTION)
-		gl.Rotated(0.1, 0, 1, 0)
-		// gl.Rotated(0.5, 0, 1, 0)
-		// gl.Rotated(0.5, 0, 1, 0)
+			w, h := glh.GetViewportWH()
+			im := image.NewRGBA(image.Rect(0, 0, w, h))
+			glh.ClearAlpha(1)
+			gl.Flush()
+			glh.CaptureRGBA(im)
 
-		// Start afresh each time
-		gl.MatrixMode(gl.MODELVIEW)
-		gl.LoadIdentity()
-		// gl.Rotated(d, 0, 1, 0)
-		_ = d
-		// d += 0.5
+			go func() {
+				fd, err := os.Create("screenshot.png")
+				if err != nil {
+					panic("Unable to open file")
+				}
+				defer fd.Close()
 
-		// Earth
-		// gl.Color4f(0, 0, 0, 0)
-		_ = q
-		glu.Sphere(q, 1, 40, 40)
+				png.Encode(fd, im)
+			}()
 
-		// unlit_points := glh.Compound(glh.Disable(gl.LIGHTING), glh.Primitive{gl.POINTS})
-		// glh.With(unlit_points, func() {
-		// 	gl.Vertex3d(1, 0, 0)
-		// })
+		case 'Q', glfw.KeyEsc:
+			running = !running
 
-		// continue
+		case glfw.KeySpace:
+			polar = !polar
+		}
+	})
+
+	render_scene := func() {
+		// Update light position (sensitive to current modelview matrix)
+		gl.Lightfv(gl.LIGHT1, gl.POSITION, light_position)
+		gl.Lightfv(gl.LIGHT2, gl.POSITION, light_position)
+
+		if earth {
+			Sphere(earth_radius, faces)
+		}
+
+		unlit_points := glh.Compound(glh.Disable(gl.LIGHTING), glh.Primitive{gl.POINTS})
+		glh.With(unlit_points, func() {
+			gl.Vertex3d(1, 0, 0)
+		})
 
 		for _, p := range planetoids {
 			const dt = 0.1 // TODO: Frame update
 			p.Render(dt)
 		}
 
-		// Atmosphere
-		gl.Disable(gl.LIGHTING)
-		gl.Color4f(0.25, 0.25, 1, 0.25)
+		glh.With(glh.Disable(gl.LIGHTING), func() {
+			// Atmosphere
+			gl.Color4f(0.25, 0.25, 1, 0.1)
 
-		glu.Sphere(q, 1.025, 100, 100)
+			if atmosphere && earth {
+				Sphere(earth_radius*1.025, 100)
+			}
 
-		gl.PointSize(10)
-		// gl.Disable(gl.DEPTH_TEST)
-		gl.Begin(gl.POINTS)
-		gl.Color4f(1.75, 0.75, 0.75, 1)
-		gl.Vertex3d(-1.04, 0, 0)
-		gl.End()
-		// gl.Enable(gl.DEPTH_TEST)
+			gl.PointSize(10)
 
-		gl.Enable(gl.LIGHTING)
+			glh.With(glh.Primitive{gl.POINTS}, func() {
+				gl.Color4f(1.75, 0.75, 0.75, 1)
+				gl.Vertex3d(-1.04, 0, 0)
+			})
+		})
 
 	}
-}
 
-func onResize(w, h int) {
-	fmt.Printf("resized: %dx%d\n", w, h)
-}
+	for running {
+		running = glfw.WindowParam(glfw.Opened) == 1
 
-func onClose() int {
-	fmt.Println("closed")
-	return 1 // return 0 to keep window open.
-}
+		glfw.SwapBuffers()
 
-func onMouseBtn(button, state int) {
-	fmt.Printf("mouse button: %d, %d\n", button, state)
-}
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
+		// gl.Clear(gl.DEPTH_BUFFER_BIT)
 
-func onMouseWheel(delta int) {
-	fmt.Printf("mouse wheel: %d\n", delta)
-}
+		gl.MatrixMode(gl.MODELVIEW)
+		gl.LoadIdentity()
+		gl.Translated(0, 0, camera_z_offset)
+		gl.Rotated(20, 1, 0, 0)
 
-func onKey(key, state int) {
-	fmt.Printf("key: %d, %d\n", key, state)
-}
+		if polar {
+			gl.Rotated(90, 1, 0, 0)
+		}
 
-func onChar(key, state int) {
-	fmt.Printf("char: %d, %d\n", key, state)
+		gl.Rotated(d, 0, -1, 0)
+		if rotating {
+			d += 1
+		}
+
+		// Plan for rendering shadows
+		// 1. render all objects into the stencil buffer except the
+
+		// gl.PointSize(100)
+		// glh.With(glh.Primitive{gl.POINTS}, func() {
+		// 	gl.Color4f(1, 1, 0, 1)
+		// 	gl.Vertex3f(0, 0, 150)
+		// })
+
+		_ = render_scene
+		render_scene()
+
+		// gl.PushAttrib(mask)
+
+		glh.With(glh.Attrib{
+			gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.ENABLE_BIT |
+				gl.POLYGON_BIT | gl.STENCIL_BUFFER_BIT,
+		}, func() {
+
+			gl.Disable(gl.LIGHTING)
+			// gl.Disable(gl.DEPTH_TEST)
+			gl.DepthMask(false)
+			gl.DepthFunc(gl.LEQUAL)
+
+			gl.Enable(gl.STENCIL_TEST)
+			gl.ColorMask(false, false, false, false)
+			gl.StencilFunc(gl.ALWAYS, 1, 0xffffffff)
+
+			// Shadow cone
+			glh.With(glh.Matrix{gl.MODELVIEW}, func() {
+				gl.Rotatef(90, 1, 0, 0)
+				gl.Rotatef(90, 0, -1, 0)
+				gl.Color4f(0.5, 0.5, 0.5, 1)
+
+				if cone {
+					gl.FrontFace(gl.CCW)
+					gl.StencilOp(gl.KEEP, gl.KEEP, gl.INCR)
+					glu.Cylinder(quadric, earth_radius*1.05, earth_radius*1.05, 5, 100, 100)
+
+					gl.FrontFace(gl.CW)
+					gl.StencilOp(gl.KEEP, gl.KEEP, gl.DECR)
+					glu.Cylinder(quadric, earth_radius*1.05, earth_radius*1.05, 5, 100, 100)
+				}
+			})
+
+			gl.StencilFunc(gl.NOTEQUAL, 0, 0xffffffff)
+			gl.StencilOp(gl.KEEP, gl.KEEP, gl.KEEP)
+
+			gl.ColorMask(true, true, true, true)
+			// gl.Disable(gl.STENCIL_TEST)
+
+			gl.Disable(gl.DEPTH_TEST)
+
+			gl.FrontFace(gl.CCW)
+			gl.Color4f(0, 0, 0, 0.75)
+			// gl.Color4f(1, 1, 1, 0.75)
+
+			gl.LoadIdentity()
+			gl.Translated(0, 0, camera_z_offset)
+			// TODO: Figure out why this doesn't draw over the whole screen
+			glh.With(glh.Disable(gl.LIGHTING), func() {
+				glh.DrawQuadd(-10, -10, 20, 20)
+			})
+		})
+
+		// gl.LoadIdentity()
+
+		// gl.Color4f(0, 0, 0, 0.7)
+
+		// render_scene()
+	}
 }
 
 func createBuffer() *glh.MeshBuffer {
