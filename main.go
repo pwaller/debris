d7ff6e8054c830370d75ee06ff5d154ef40e3a61
Peter Waller <peter@scraperwiki.com>
2013-12-27 10:37:44 +0000

    Give planetoids their own shadow volume.. and stuff.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"unsafe"
 
@@ -54,12 +55,12 @@ func (p *Planetoid) Render(dp float64) {
 		gl.Rotated(p.inclination, 0, 0, 1)
 
 		gl.Rotated(p.phase0+p.phase, 0, 1, 0)
-		p.phase += dp
+		p.phase += dp*10 + (1 - p.apogee)
 
 		glh.With(glh.Matrix{gl.MODELVIEW}, func() {
 			// TODO: Compute position correctly
 			gl.Translated(p.apogee, 0, 0)
-			Sphere(p.radius, 20)
+			Sphere(p.radius, 3)
 		})
 
 		// TODO: Avoid depth thrashing when trails overlap
@@ -76,6 +77,30 @@ func (p *Planetoid) Render(dp float64) {
 	})
 }
 
+func (p *Planetoid) RenderShadowVolume() {
+	glh.With(glh.Matrix{gl.MODELVIEW}, func() {
+
+		// gl.Rotated(90, 0, 1, 0)
+
+		gl.Rotated(p.rising_node, 0, 1, 0)
+		gl.Rotated(p.inclination, 0, 0, 1)
+
+		gl.Rotated(p.phase0+p.phase, 0, 1, 0)
+
+		// TODO: Compute position correctly
+		gl.Translated(p.apogee, 0, 0)
+
+		gl.Rotated(p.phase0+p.phase, 0, -1, 0)
+		gl.Rotated(p.inclination, 0, 0, -1)
+		gl.Rotated(p.rising_node, 0, -1, 0)
+
+		// gl.Rotated(p.phase0+p.phase, 0, -1, 0)
+
+		gl.Rotated(90, 0, -1, 0)
+		glu.Cylinder(quadric, p.radius, p.radius, 2, 10, 1)
+	})
+}
+
 func main() {
 	var err error
 	if err = glfw.Init(); err != nil {
@@ -86,6 +111,7 @@ func main() {
 	defer glfw.Terminate()
 
 	w, h := 1980, 1080
+	// w, h := 1280, 768
 	if err = glfw.OpenWindow(w, h, 8, 8, 8, 16, 0, 32, glfw.Fullscreen); err != nil {
 		fmt.Fprintf(os.Stderr, "[e] %v\n", err)
 		return
@@ -124,12 +150,14 @@ func main() {
 	)
 
 	const (
-		fov               = 1 // degrees
+		fov               = 1.1 // degrees
 		znear             = 145
 		zfar              = 155
 		camera_z_offset   = -150
 		camera_x_rotation = 0 // degrees
 		// camera_x_rotation = 20 // degrees
+
+		starfield_fov = 45
 
 		faces        = 1000
 		earth_radius = 1
@@ -154,17 +182,18 @@ func main() {
 	b := createBuffer()
 
 	planetoids := []*Planetoid{}
-	for i := 0; i < 10; i++ {
-		r := 0.05 * (math.Cos(float64(i)*79) + 1)
-		// p := NewPlanetoid(r, 1.5, 1.5, 0, , b)
+	for i := 0; i < 1000; i++ {
 		p := &Planetoid{
-			apogee:  1.2,
+			apogee:  1.2 + rand.Float64()*0.7,
 			perigee: 1.5,
 			// inclination: 45,
-			inclination: 0,
-			phase0:      float64(i) * 57.1,
+			inclination: rand.Float64()*20 - 10,
+			// inclination: 0,
+			phase0:      rand.Float64() * 360,
+			rising_node: rand.Float64() * 10,
 			phase:       0,
-			radius:      float32(r),
+			// radius:      rand.Float32()*0.05 + 0.01, //float32(r),
+			radius: rand.Float32()*0.0125 + 0.005, //float32(r),
 			// quadric:     glu.NewQuadric(),
 			circle: b,
 		}
@@ -173,18 +202,28 @@ func main() {
 
 	// Initial projection matrix:
 
-	gl.MatrixMode(gl.PROJECTION)
-	glu.Perspective(fov, float64(w)/float64(h), znear, zfar)
+	var aspect float64
+
+	glfw.SetWindowSizeCallback(func(w, h int) {
+		gl.Viewport(0, 0, w, h)
+
+		gl.MatrixMode(gl.PROJECTION)
+		gl.LoadIdentity()
+		aspect = float64(w) / float64(h)
+		glu.Perspective(fov, aspect, znear, zfar)
+	})
 
 	d := float64(0)
 
 	wireframe := false
-	atmosphere := true
+	atmosphere := false
 	polar := false
 	rotating := false
 	front := false
 	earth := true
 	cone := true
+	shadowing := true
+	tilt := false
 	running := true
 
 	glfw.SetKeyCallback(func(key, state int) {
@@ -209,6 +248,10 @@ func main() {
 			} else {
 				gl.FrontFace(gl.CCW)
 			}
+		case 'S':
+			shadowing = !shadowing
+		case 'T':
+			tilt = !tilt
 		case 'W':
 			wireframe = !wireframe
 			method := gl.GLenum(gl.FILL)
@@ -245,7 +288,52 @@ func main() {
 		}
 	})
 
+	_ = rand.Float64
+
+	stars := glh.NewMeshBuffer(
+		glh.RenderArrays,
+		glh.NewPositionAttr(3, gl.DOUBLE, gl.STATIC_DRAW),
+		glh.NewColorAttr(3, gl.DOUBLE, gl.STATIC_DRAW))
+
+	const Nstars = 50000
+	points := make([]float64, 3*Nstars)
+	colors := make([]float64, 3*Nstars)
+
+	for i := 0; i < Nstars; i++ {
+		const R = 1
+
+		phi := rand.Float64() * 2 * math.Pi
+		z := R * (2*rand.Float64() - 1)
+		theta := math.Asin(z / R)
+
+		points[i*3+0] = R * math.Cos(theta) * math.Cos(phi)
+		points[i*3+1] = R * math.Cos(theta) * math.Sin(phi)
+		points[i*3+2] = z
+
+		const r = 0.8
+		v := rand.Float64()*r + (1 - r)
+		colors[i*3+0] = v
+		colors[i*3+1] = v
+		colors[i*3+2] = v
+	}
+
+	stars.Add(points, colors)
+
+	render_stars := func() {
+		glh.With(glh.Attrib{gl.DEPTH_BUFFER_BIT | gl.ENABLE_BIT}, func() {
+			gl.Disable(gl.LIGHTING)
+			gl.PointSize(1)
+			gl.Color4f(1, 1, 1, 1)
+
+			gl.Disable(gl.DEPTH_TEST)
+			gl.DepthMask(false)
+
+			stars.Render(gl.POINTS)
+		})
+	}
+
 	render_scene := func() {
+
 		// Update light position (sensitive to current modelview matrix)
 		gl.Lightfv(gl.LIGHT1, gl.POSITION, light_position)
 		gl.Lightfv(gl.LIGHT2, gl.POSITION, light_position)
@@ -282,41 +370,7 @@ func main() {
 
 	}
 
-	for running {
-		running = glfw.WindowParam(glfw.Opened) == 1
-
-		glfw.SwapBuffers()
-
-		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
-		// gl.Clear(gl.DEPTH_BUFFER_BIT)
-
-		gl.MatrixMode(gl.MODELVIEW)
-		gl.LoadIdentity()
-		gl.Translated(0, 0, camera_z_offset)
-		gl.Rotated(20, 1, 0, 0)
-
-		if polar {
-			gl.Rotated(90, 1, 0, 0)
-		}
-
-		gl.Rotated(d, 0, -1, 0)
-		if rotating {
-			d += 1
-		}
-
-		// Plan for rendering shadows
-		// 1. render all objects into the stencil buffer except the
-
-		// gl.PointSize(100)
-		// glh.With(glh.Primitive{gl.POINTS}, func() {
-		// 	gl.Color4f(1, 1, 0, 1)
-		// 	gl.Vertex3f(0, 0, 150)
-		// })
-
-		_ = render_scene
-		render_scene()
-
-		// gl.PushAttrib(mask)
+	render_shadow_volume := func() {
 
 		glh.With(glh.Attrib{
 			gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.ENABLE_BIT |
@@ -324,71 +378,148 @@ func main() {
 		}, func() {
 
 			gl.Disable(gl.LIGHTING)
-			// gl.Disable(gl.DEPTH_TEST)
-			gl.DepthMask(false)
-			gl.DepthFunc(gl.LEQUAL)
 
-			gl.Enable(gl.STENCIL_TEST)
-			gl.ColorMask(false, false, false, false)
-			gl.StencilFunc(gl.ALWAYS, 1, 0xffffffff)
+			if shadowing {
+				// gl.Disable(gl.DEPTH_TEST)
+				gl.DepthMask(false)
+				gl.DepthFunc(gl.LEQUAL)
 
-			// Shadow cone
-			glh.With(glh.Matrix{gl.MODELVIEW}, func() {
-				gl.Rotatef(90, 1, 0, 0)
-				gl.Rotatef(90, 0, -1, 0)
-				gl.Color4f(0.5, 0.5, 0.5, 1)
+				gl.Enable(gl.STENCIL_TEST)
+				gl.ColorMask(false, false, false, false)
+				gl.StencilFunc(gl.ALWAYS, 1, 0xffffffff)
+			}
 
-				if cone {
-					gl.FrontFace(gl.CCW)
-					gl.StencilOp(gl.KEEP, gl.KEEP, gl.INCR)
-					glu.Cylinder(quadric, earth_radius*1.05, earth_radius*1.05, 5, 100, 100)
+			shadow_volume := func() {
+				const sv_length = 2
+				const sv_granularity = 100
+				const sv_radius = earth_radius * 1.001
 
-					gl.FrontFace(gl.CW)
-					gl.StencilOp(gl.KEEP, gl.KEEP, gl.DECR)
-					glu.Cylinder(quadric, earth_radius*1.05, earth_radius*1.05, 5, 100, 100)
+				// Shadow cone
+				glh.With(glh.Matrix{gl.MODELVIEW}, func() {
+					gl.Rotatef(90, 1, 0, 0)
+					gl.Rotatef(90, 0, -1, 0)
+					gl.Color4f(0.5, 0.5, 0.5, 1)
+					glu.Cylinder(quadric, sv_radius, sv_radius*1.05,
+						sv_length, sv_granularity, 1)
+
+					glu.Disk(quadric, 0, sv_radius, sv_granularity, 1)
+
+					glh.With(glh.Matrix{gl.MODELVIEW}, func() {
+						gl.Translated(0, 0, sv_length)
+						glu.Disk(quadric, 0, sv_radius*1.05, sv_granularity, 1)
+					})
+				})
+
+				for _, p := range planetoids {
+					p.RenderShadowVolume()
 				}
-			})
 
-			gl.StencilFunc(gl.NOTEQUAL, 0, 0xffffffff)
-			gl.StencilOp(gl.KEEP, gl.KEEP, gl.KEEP)
+			}
 
-			gl.ColorMask(true, true, true, true)
-			// gl.Disable(gl.STENCIL_TEST)
+			if cone {
+				gl.FrontFace(gl.CCW)
+				gl.StencilOp(gl.KEEP, gl.KEEP, gl.INCR)
 
-			gl.Disable(gl.DEPTH_TEST)
+				shadow_volume()
 
-			gl.FrontFace(gl.CCW)
-			gl.Color4f(0, 0, 0, 0.75)
-			// gl.Color4f(1, 1, 1, 0.75)
+				gl.FrontFace(gl.CW)
+				gl.StencilOp(gl.KEEP, gl.KEEP, gl.DECR)
 
+				shadow_volume()
+			}
+
+			if shadowing {
+				gl.StencilFunc(gl.NOTEQUAL, 0, 0xffffffff)
+				gl.StencilOp(gl.KEEP, gl.KEEP, gl.KEEP)
+
+				gl.ColorMask(true, true, true, true)
+				// gl.Disable(gl.STENCIL_TEST)
+
+				gl.Disable(gl.DEPTH_TEST)
+
+				gl.FrontFace(gl.CCW)
+				// gl.Color4f(1, 0, 0, 0.75)
+				gl.Color4f(0, 0, 0, 0.75)
+				// gl.Color4f(1, 1, 1, 0.75)
+
+				gl.LoadIdentity()
+				gl.Translated(0, 0, camera_z_offset)
+				// TODO: Figure out why this doesn't draw over the whole screen
+				glh.With(glh.Disable(gl.LIGHTING), func() {
+					glh.DrawQuadd(-10, -10, 20, 20)
+				})
+
+				// gl.FrontFace(gl.CW)
+				// gl.Enable(gl.LIGHTING)
+				// gl.Disable(gl.LIGHT1)
+				// render_scene()
+				// gl.Enable(gl.LIGHT1)
+			}
+		})
+	}
+	_ = render_stars
+
+	for running {
+		running = glfw.WindowParam(glfw.Opened) == 1
+
+		glfw.SwapBuffers()
+
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
+
+		rotation := func() {
+			if tilt {
+				gl.Rotated(20, 1, 0, 0)
+			}
+
+			if polar {
+				gl.Rotated(90, 1, 0, 0)
+			}
+
+			gl.Rotated(d, 0, -1, 0)
+		}
+
+		// Star field
+
+		glh.With(glh.Matrix{gl.PROJECTION}, func() {
 			gl.LoadIdentity()
-			gl.Translated(0, 0, camera_z_offset)
-			// TODO: Figure out why this doesn't draw over the whole screen
-			glh.With(glh.Disable(gl.LIGHTING), func() {
-				glh.DrawQuadd(-10, -10, 20, 20)
+			glu.Perspective(starfield_fov, aspect, 0, 1)
+
+			glh.With(glh.Matrix{gl.MODELVIEW}, func() {
+				gl.LoadIdentity()
+				rotation()
+				render_stars()
 			})
 		})
 
-		// gl.LoadIdentity()
+		gl.MatrixMode(gl.MODELVIEW)
+		gl.LoadIdentity()
+		gl.Translated(0, 0, camera_z_offset)
 
-		// gl.Color4f(0, 0, 0, 0.7)
+		rotation()
+		if rotating {
+			d += 0.2
+		}
 
-		// render_scene()
+		_ = render_scene
+		render_scene()
+
+		_ = render_shadow_volume
+		render_shadow_volume()
 	}
 }
 
 func createBuffer() *glh.MeshBuffer {
 	const N = 128
-	const f = 0.05
+	const f = 0.025
 	pos := make([]float64, N*2)
 	clr := make([]float64, N*4)
 	for i := 0; i < N; i += 1 {
-		pos[2*i] = math.Cos(f * 2 * math.Pi * 2 * float64(i) / N)
+		pos[2*i+0] = math.Cos(f * 2 * math.Pi * 2 * float64(i) / N)
 		pos[2*i+1] = math.Sin(f * 2 * math.Pi * 2 * float64(i) / N)
-		clr[4*i] = 0.75
-		clr[4*i+1] = 0.2
+		clr[4*i+0] = 0.2
+		clr[4*i+1] = 0.75
 		clr[4*i+2] = 0.9
-		clr[4*i+3] = 1 - float64(i)/float64(N)
+		clr[4*i+3] = 0.5 * (1 - float64(i)/float64(N-1))
 	}
 
 	// Create a mesh buffer with the given attributes.
